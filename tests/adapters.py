4ba3a3ea688fadf007e49f79fa09936cb9f6eaf5
echung32 <143231894+echung32@users.noreply.github.com>
2026-02-11 15:45:24 -0600

    implement run_bpe_train

--- tests/adapters.py
+++ tests/adapters.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import os
 from collections.abc import Iterable
-from typing import IO, Any, BinaryIO
+from typing import IO, Any, BinaryIO, Counter
 
 import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
+import regex as re
+from tqdm import tqdm
 
 
 def run_linear(
@@ -562,6 +564,101 @@ def get_tokenizer(
     raise NotImplementedError
 
 
+def _find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), "Must represent special token as a bytestring"
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
+
+
+# GPT-2 pre-tokenizer
+_GPT2_PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+
+def _process_chunk(path: str, start: int, end: int, pattern: str):
+    frequency: dict[tuple[bytes, ...], int] = Counter()
+
+    with open(path, "rb") as f:
+        f.seek(start)
+        text = f.read(end - start).decode("utf-8", errors="ignore")
+
+    # Run pre-tokenization on your chunk and store the counts for each pre-token
+    documents = re.split(pattern, text)
+    # each batch has multiple documents, split by <|endoftext|> for many documents
+    for doc in documents:
+        # run tokenizer on each document individually
+        for m in re.finditer(_GPT2_PAT, doc):
+            tok = m.group(0)
+            tok_bytes = tok.encode("utf-8")
+            # It is convenient to represent this as a dict[tuple[bytes], int], e.g. {(l,o,w): 5 …}.
+            tok_tuple = tuple(bytes([b]) for b in tok_bytes)
+            frequency[tok_tuple] += 1
+
+    return frequency
+
+
+def _pretokenize(path: str, num_processes: int, special_tokens: list[str]) -> dict[tuple[bytes, ...], int]:
+    import multiprocessing as mp
+    
+    with open(path, "rb") as f:
+        boundaries = _find_chunk_boundaries(f, num_processes, special_tokens[0].encode("utf-8") if special_tokens else b"\n")
+
+    # escapes the | in the special tokens
+    pattern = "|".join([re.escape(tok) for tok in special_tokens])
+
+    # create chunks and run pre-tokenization
+    tasks = [(path, start, end, pattern) for start, end in zip(boundaries[:-1], boundaries[1:])]
+    with mp.Pool(processes=num_processes) as pool:
+        result = pool.starmap(_process_chunk, tasks)
+
+        # combine the individual counters
+        total_frequency: dict[tuple[bytes, ...], int] = Counter()
+        for freq in result:
+            total_frequency.update(freq)
+
+    return total_frequency
+
+
 def run_train_bpe(
     input_path: str | os.PathLike,
     vocab_size: int,
@@ -590,6 +687,92 @@ def run_train_bpe(
                 Merges are ordered by order of creation.
     """
 
-    with open(input_path, "rb") as f:
-        corpus = f.read()
+    import multiprocessing as mp
+    num_processes = min(mp.cpu_count(), 8) 
+    
+    frequency = _pretokenize(str(input_path), num_processes, special_tokens)
+    print(len(frequency), "unique pre-tokens found in the dataset.")
+    
+    ##### ---- bpe merging now
+    # cannot do the merge parallel, as instructions state.
+    
+    merges: list[tuple[bytes, bytes]] = []
+    vocab: dict[int, bytes] = {}
+    vocab_idx = 0
+
+    # first init with the special tokens
+    for i, tok in enumerate(special_tokens):
+        vocab[vocab_idx] = tok.encode("utf-8")
+        vocab_idx += 1
+
+    # then add our initial vocab list
+    # these represent the initial range from 0-255
+    # https://www.ascii-code.com/
+    for b in range(256):
+        vocab[vocab_idx] = bytes([b])
+        vocab_idx += 1
+
+    # keep track of the pairs (like l,o ; o,w and the counts
+    pair_counts: dict[tuple[bytes, bytes], int] = Counter()
+
+    for tok, count in frequency.items():
+        if len(tok) == 1:
+            continue # nothing to merge
+        for b1, b2 in zip(tok[:-1], tok[1:]):
+            pair_counts[(b1, b2)] += count
+
+    while len(vocab) < vocab_size:
+        if not pair_counts:
+            # No more pairs to merge
+            break
+            
+        # now we are processing merges, find the most frequent pair
+        # take lexicographical max to break ties, x[1] is value, then x[0] is key
+        most_frequent_pair = max(pair_counts.items(), key=lambda x: (x[1], x[0]))[0]
+
+        # update the vocab now
+        vocab[vocab_idx] = most_frequent_pair[0] + most_frequent_pair[1]
+        vocab_idx += 1
+        merges.append(most_frequent_pair)
+        
+        # with the most frequent pair, merge their occurrences in total_frequency
+        # need to create copy as a list because modifying dict directly
+        for tok, count in list(frequency.items()):
+            # maintain list of new tokens to create at the end
+            _new_tok: list[bytes] = []
+            idx = 0
+            merged = False
+
+            while idx < len(tok):
+                # build new token by going thru all pairs first (vs. editing while going thru)
+                if idx < len(tok) - 1 and (tok[idx], tok[idx+1]) == most_frequent_pair:
+                    _new_tok.append(tok[idx] + tok[idx+1])
+                    merged = True
+                    idx += 2 # skip 2 because we merged 2 into 1
+                else:
+                    # case where there's no match
+                    _new_tok.append(tok[idx])
+                    idx += 1
+
+            new_tok: tuple[bytes, ...] = tuple(_new_tok)
+            # now update the pair counts if there was a merge
+            if merged:
+                # todo: can do in one for loop later but this was just a bit more intuitive
+                tok_pairs = [(tok[i], tok[i+1]) for i in range(len(tok)-1)]
+                new_tok_pairs = [(new_tok[i], new_tok[i+1]) for i in range(len(new_tok)-1)]
+
+                for old_pair in tok_pairs:
+                    pair_counts[old_pair] -= count
+                    if pair_counts[old_pair] <= 0: del pair_counts[old_pair]
+
+                for new_pair in new_tok_pairs:
+                    pair_counts[new_pair] += count
+
+                # update the frequency counter
+                frequency[tok] -= count
+                if frequency[tok] <= 0: del frequency[tok]
+                # cant += on something not defined yet, so use get+count instead
+                frequency[new_tok] = frequency.get(new_tok, 0) + count
+    
+    return vocab, merges
 
